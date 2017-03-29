1336ffd2d9d4668ded89458a8071d57a6cc5893d
ryvolum <ryvolum@microsoft.com>
2017-03-29 14:48:33 -0700

    half-baked sell functionality

--- app.js
+++ app.js
@@ -10,22 +10,22 @@ bot.dialog('promptButtons', [
     (session, results) => {
         switch (results.response.entity) {
             case "Buy Stock":
-                session.replaceDialog('/buyStock');
+                session.replaceDialog('buyStock');
                 break;
             case "Check Investment Status":
-                session.replaceDialog('/checkStocks');
+                session.replaceDialog('checkStocks');
                 break;
-            case "Remove Stock":
-                session.replaceDialog('/sellStock');
+            case "Sell Stock":
+                session.replaceDialog('sellStock');
                 break;
             case "Lookup Stock":
-                session.replaceDialog("/lookupStock");
+                session.replaceDialog("lookupStock");
                 break;
         }
     }
 ])
 
-bot.dialog("/buyStock", [
+bot.dialog("buyStock", [
     (session) => {
         builder.Prompts.text(session, "What stock (ticker symbol) would you like to buy?");
     },
@@ -56,16 +56,107 @@ bot.dialog("/buyStock", [
     }
 ])
 
+//TODO: Combine sellStock and buyStock dialogs into one that receives a parameter for buy/sell and forks logic internally
+bot.dialog('sellStock', [
+    (session) => {
+        builder.Prompts.text(session, "What stock (ticker symbol) would you like to sell?");
+    },
+    (session, results) => {
+        var ticker = results.response;
+        getStockInfo(ticker, (stockInfo, error) => {
+            if (stockInfo && stockInfo.LastPrice && stockInfo.Name && !error) {
+                session.dialogData.currentStockInfo = stockInfo;
+                builder.Prompts.number(session, "The current price of " + stockInfo.Name + " is " + stockInfo.LastPrice + ". How much ya lookin' to sell?")
+            } else {
+                session.endDialog("Hmm I'm not sure that's a real ticker symbol. Either I'm broken or that's not an actual ticker symbol...")
+            }
+        })
+    },
+    (session, results) => {
+        var stockQuantity = results.response;
+        if (stockQuantity > 0) {
+            //TODO: Resolve potential vulnerability - we should re-call the price API before we make purchases. 
+            var sellPrice = stockQuantity * session.dialogData.currentStockInfo.LastPrice;
+            if (removeStocks(session, session.dialogData.currentStockInfo.Symbol, stockQuantity)) {
+                session.privateConversationData.cash += sellPrice;
+                session.endDialog("Congrats! You just sold " + stockQuantity + " units of " + session.dialogData.currentStockInfo.Name + " stock. You've got $" + session.privateConversationData.cash + " now.")
+            } else {
+                session.endDialog("Couldn't sell that many of that stock. Don't think you've got that amount of that stock to sell...");
+            }
+        }
+    }
+])
+
+bot.dialog('checkStocks', [
+])
+
+bot.dialog('lookupStock', [
+])
+
 let addStocks = (session, quantity) => {
-    if (!session.privateConversationData.stocks)
+    if (!session.privateConversationData.stocks) {
         session.privateConversationData.stocks = [];
-    session.privateConversationData.stocks.push({ name: session.dialogData.currentStockInfo.Name, quantity: quantity });
-    return;
+    }
+    if (hasStock(session)) {
+        var stockIndex = getStockIndex(session.dialogData.currentStockInfo.Symbol);
+        session.privateConversationData.stocks[i].quantity += quantity;
+    } else {
+        session.privateConversationData.stocks.push({ name: session.dialogData.currentStockInfo.Symbol, quantity: quantity });
+    }
+}
+
+let getQuantityOfStock = (session, ticker) => {
+    session.privateConversationData.stocks.forEach((stock, i) => {
+        if (session.privateConversationData.stocks[i].ticker === ticker) {
+            if (session.privateConversationData.stocks[i].quantity)
+                return session.privateConversationData.stocks[i].quantity;
+            return -1;
+        }
+    })
+}
+
+let removeStocks = (session, ticker, quantity) => {
+    //Probably want more granularity in response. Right now we return false for both non-existent stocks and for exceeded quantity sold
+    var index = getStockIndex(session, ticker);
+    if (index > -1) {
+        if (quantity < session.privateConversationData.stocks[i].quantity) {
+            session.privateConversationData.stocks[i].quantity -= quantity;
+        } else if (quantity === session.privateConversationData.stocks[i].quantity) {
+            //Deletes stock object
+            session.privateConversationData.stocks.splice(index, 1);
+        } else {
+            return false;
+        }
+    } else {
+        return false;
+    }
+    return true;
+}
+
+let getStockIndex = (session, ticker) => {
+    session.privateConversationData.stocks.forEach((stock, i) => {
+        if (session.privateConversationData.stocks[i].name === ticker) {
+            return i;
+        }
+        if (i === session.privateConversationData.stocks.length - 1)
+            return false;
+    })
+}
+
+let hasStock = (session, ticker) => {
+    session.privateConversationData.stocks.forEach((stock, i) => {
+        if (session.privateConversationData.stocks[i].name === ticker) {
+            return true;
+        }
+        if (i === session.privateConversationData.stocks.length - 1)
+            return false;
+    })
 }
 
 let getStockInfo = (ticker, callback) => {
     request('http://dev.markitondemand.com/MODApis/Api/v2/Quote/jsonp?symbol=' + ticker, (error, response, body) => {
         if (!error && response.statusCode == 200) {
+            //Some hacky parsing. Should probably use a real API :)
             callback(JSON.parse(body.substring(18, body.length - 1)), null);
         } else {
             callback(null, error);
